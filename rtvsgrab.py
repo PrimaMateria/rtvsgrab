3ebe4ec0459a93200051a8ee7b3d35a01ca5270f
matus.benko <matus.benko@gmail.com>
2020-11-30 00:38:34 +0100

    refactor to functions

--- rtvsgrab.py
+++ rtvsgrab.py
@@ -15,16 +15,25 @@ def grab(url):
     source = response.data.decode('utf-8')
     urlline = re.findall(r"^.*var url =.*$", source, re.MULTILINE)[0]
     video_metadata_url = re.match(r"^.*var url = \"(.*)\".*$", urlline)[1]
+    video_data = get_video_data(video_metadata_url)
+    save_video(video_data['date'], video_data['src'])
 
-    metadata = json.loads(http.request(
-        "GET", video_metadata_url).data.decode('utf-8'))
-    date = metadata['clip']['datetime_create'].split()[0]
-    videosource = next(filter(
-        lambda s: s['type'] == 'application/x-mpegurl', metadata['clip']['sources']))['src']
-    videofilename = '/mnt/caladan/rtvs/'+date+'.mkv'
 
-    ffmpeg.input(videosource).output(
-        videofilename, vcodec='copy', acodec='copy').run(quiet=True)
+def save_video(date, source):
+    file_path = '/mnt/caladan/rtvs/' + date + '.mkv'
+    ffmpeg.input(source).output(file_path, vcodec='copy',
+                                acodec='copy').run()
+
+
+def get_video_data(url):
+    metadata = json.loads(http.request("GET", url).data.decode('utf-8'))
+    source = metadata['clip']['sources']
+    data = dict()
+    def type_filter(s): return s['type'] == 'application/x-mpegurl'
+    data['src'] = next(filter(type_filter, sources))['src']
+    data['date'] = metadata['clip']['datetime_create'].split()[0]
+
+    return data
 
 
 if __name__ == "__main__":
